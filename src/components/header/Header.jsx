d33e22911313e13170675529da65bd2b1034f550
yuna <yuna744@gmail.com>
2024-01-15 15:34:50 +0900

    0115 로그인 버튼 컴포넌트 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import styles from './header.module.scss';
 import Link from 'next/link';
 import Navbar from '../navbar/Navbar';
 import dynamic from 'next/dynamic';
+import BtnLogin from '../btnLogin/BtnLogin';
 
 // 기존 clinet 방식의 컴포넌트를 import 시 ssr: false를 통해 서버쪽에서 pre-build되지 않도록 설정
 // const NoSsrNavbar = dynamic(() => import('@/components/navbar/Navbar'), { ssr: false }); //ssr 서버사이드 랜더링 방식: false
@@ -18,6 +19,8 @@ export default function Header() {
 
 			<Navbar textArr={['about', 'youtube', 'post']} />
 			{/* <NoSsrNavbar textArr={['about', 'youtube', 'post']} /> */}
+
+			<BtnLogin session={false} />
 		</header>
 	);
 }
