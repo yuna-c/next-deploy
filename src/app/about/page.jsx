21aa81de1552d962a268bb669ac7ccf5148d5288
yuna <yuna744@gmail.com>
2024-01-15 15:49:39 +0900

    0115 page컴포넌트에서 에러 발생시 출력할 페이지 커스터마이징

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,6 +3,8 @@ import styles from './about.module.scss';
 // npm i sass clsx
 
 export default function About() {
+	throw new Error('Error in Qbout page...');
+
 	return (
 		<section className={clsx(styles.about)}>
 			<h1>About</h1>
---
+++ src/app/error.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Error() {
+	return (
+		<section>
+			<h1>404 There is No page you requested</h1>
+		</section>
+	);
+}
