9d0bacb05ddcb8d84ec9d9c0eb44a4cb1e9c210f
yuna <yuna744@gmail.com>
2024-01-15 16:39:23 +0900

    0115 btnLogin 우선 순위 해결, header 오른쪽 메뉴버튼들 그룹화

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,10 @@ import styles from './header.module.scss';
 import Link from 'next/link';
 import Navbar from '../navbar/Navbar';
 import BtnLogin from '../btnLogin/BtnLogin';
+import { FaBarsStaggered } from 'react-icons/fa6';
 import dynamic from 'next/dynamic';
 
+// npm i react-icons
 // 기존 clinet 방식의 컴포넌트를 import 시 ssr: false를 통해 서버쪽에서 pre-build되지 않도록 설정
 // const NoSsrNavbar = dynamic(() => import('@/components/navbar/Navbar'), { ssr: false }); //ssr 서버사이드 랜더링 방식: false
 
@@ -15,8 +17,11 @@ export default function Header() {
 				<Link href='/'>DCODELAB</Link>
 			</h1>
 
-			<Navbar textArr={['about', 'youtube', 'post']} />
-			<BtnLogin session={true} />
+			<div>
+				<Navbar textArr={['about', 'youtube', 'post']} />
+				<BtnLogin session={true} />
+				<FaBarsStaggered size={20} color={'#333'} />
+			</div>
 		</header>
 	);
 }
