ebe898dba746c61a7e528067418defac8dba563d
yuna <yuna744@gmail.com>
2024-01-15 17:32:13 +0900

    0115 서버에는 nav, header 둘다 빌드되는 이유

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,16 +10,22 @@ import dynamic from 'next/dynamic';
 // 기존 clinet 방식의 컴포넌트를 import 시 ssr: false를 통해 서버쪽에서 pre-build되지 않도록 설정
 // const NoSsrNavbar = dynamic(() => import('@/components/navbar/Navbar'), { ssr: false }); //ssr 서버사이드 랜더링 방식: false
 
+// https://velog.io/@fervor_dev/Next.js-usePathname-useSearchParams
+// 'use client'; 짝꿍 +
+// import { usePathname } from 'next/navigation';
+// usePathname: 현재 url을 확인할 수 있는 훅, 쿼리스트링 값은 제외하고 가져옴, 동적라우팅
+
 export default function Header() {
+	console.log('header');
+
 	return (
 		<header className={clsx(styles.header)}>
 			<h1>
 				<Link href='/'>DCODELAB</Link>
 			</h1>
-
 			<Navbar textArr={['about', 'youtube', 'post']} />
-			<BtnLogin session={true} />
-			<FaBarsStaggered className={clsx(styles.btnMenu)} size={'20'} color={'#333'} />
+			<BtnLogin session={true} /> {/* 서버사이드 랜더링 */}
+			<FaBarsStaggered className={clsx(styles.btnMenu)} size={'20'} color={'#333'} /> {/* 서버사이드 랜더링 */}
 		</header>
 	);
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 'use client';
 //client 방식으로 컴포넌트를 설정해도 초기 한번은 서버쪽에서 렌더링된 다음에 넘어옴
-//hyration: 정적인 데이터 일단은 기능없이 미리 서버쪽에서 pre-render(pre-biuld)해서 출력한 다음 클라이언트가 동작할 준비가 되면 그때 클라이언트 기능을 활용할 수 있는 동적인 컴포넌트로 변경처리
+//hyration: 정적인 데이터 일단은 기능없이 미리 서버쪽에서 pre-render(pre-biuld : 빌드하는 시점에 가라로 만들어서 모냥만 보내주고)해서 출력한 다음 클라이언트가 동작할 준비가 되면 그때 클라이언트 기능을 활용할 수 있는 동적인 컴포넌트로 변경처리(컴포넌트 다 마운트되면 데이터 갈아 끼기)
 //주의점: 서버쪽에서 렌더링된 결과값과 초기 클라이언트에서 동작되는 값이 동일해야됨
+
 //해결방법1: useEffect를 이용해서 컴포넌트가 마운트될때에만 특정 state값을 활성화시키고 해당값이 활성화될때에만 클라이언트에서 활용한 값을 호출하는 방법
 //해결방법2: Dinamic import 방식을 활용해서 client 방식으로 동작하는 컴포넌트를 애초에 서버쪽으로 rebuild되지 않도록 처리
 import clsx from 'clsx';
@@ -12,8 +13,9 @@ import { useEffect, useState } from 'react'; //해결방법 1
 import { useCustomText } from '@/hooks/useText';
 
 export default function Navbar({ textArr }) {
+	console.log('nav');
 	const time = new Date().getTime();
-	const pathName = usePathname();
+	const pathName = usePathname(); //서버에 빌드 되고나서 한번 호출 되고 , 클라이언트에서 하이드레이션되는 것( 그래서 서버에서 한번은 프리 랜더가 됌)
 	const [Client, setClient] = useState(false); //해결방법 1
 	const setCapitalize = useCustomText('capitalize');
 
