9dda7ceec94e5ae7eff51fe5a2d384688a1150af
yuna <yuna744@gmail.com>
2024-01-16 11:48:55 +0900

    0116 youtube 목록화면 스타일링

--- src/app/youtube/page.jsx
+++ src/app/youtube/page.jsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 
 async function fetchYoutube() {
 	const api_key = 'AIzaSyDC60bIIkAJFzy7ji4a0Eo3AX6tYudhe1w';
+	//const pid = process.env.REACT_APP_YOUTUBE_LIST;
 	const pid = 'PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W';
 	const num = 10;
 	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
@@ -26,16 +27,6 @@ export default async function Youtube() {
 
 				return (
 					<article key={data.id}>
-						<h2>{shortenText(data.snippet.title, 50)}</h2>
-
-						<div className={styles.txt}>
-							<p>{shortenText(data.snippet.description, 250)}</p>
-							<div className={styles.infoBox}>
-								<span>{customText(date, '.')}</span>
-								<em>{time.split('Z')[0]}</em>
-							</div>
-						</div>
-
 						<div className={styles.pic}>
 							<Link href={`/detail/${data.id}`}>
 								{/* 외부 이미지 연결시 next.config파일의 이미지 protocol, hostname등록, fill, sizes, priority등록 */}
@@ -48,6 +39,15 @@ export default async function Youtube() {
 									priority
 								/>
 							</Link>
+						</div>
+						<h2>{shortenText(data.snippet.title, 50)}</h2>
+
+						<div className={styles.txt}>
+							<p>{shortenText(data.snippet.description, 250)}</p>
+							<div className={styles.infoBox}>
+								<span>{customText(date, '.')}</span>
+								{/* <em>{time.split('Z')[0]}</em> */}
+							</div>
 						</div>
 					</article>
 				);
