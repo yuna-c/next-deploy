e7cd08fcda1844b8fbcce4ed05f7967f83610ef3
yuna <yuna744@gmail.com>
2024-01-22 12:11:14 +0900

    0122 upload 이미지 파일 기능 추가

--- next.config.js
+++ next.config.js
@@ -17,7 +17,8 @@ const nextConfig = {
 			{ protocol: 'https', hostname: 'live.staticflickr.com' },
 			{ protocol: 'https', hostname: 'avatars.githubusercontent.com' },
 			{ protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-			{ protocol: 'https', hostname: 'lh3.googleusercontent.com' }
+			{ protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+			{ protocol: 'https', hostname: 'utfs.io' }
 		]
 	}
 };
--- src/components/uploadImage/UploadImage.jsx
+++ src/components/uploadImage/UploadImage.jsx
@@ -1,13 +1,16 @@
 'use client';
 const { UploadButton } = require('@uploadthing/react');
+import { useGlobalData } from '@/hooks/useGlobalData';
 
 export default function ImageUploader() {
+	const { setImgUrl } = useGlobalData();
 	return (
 		<div>
 			<UploadButton
 				endpoint='imageUploader'
 				onClientUploadComplete={res => {
 					console.log('Files: ', res);
+					setImgUrl(res[0].url);
 					alert('Upload Completed');
 				}}
 				onUploadError={error => {
